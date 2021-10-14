927cdc3fc60f0a0142ed1637dec4c4258bafdee5
nymphaea-v1 <roselin.6@yandex.ru>
2021-10-14 19:34:03 +0300

    client host/port

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,10 +1,22 @@
 package client;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 
 public class Main {
     public static void main(String[] args) {
-        ServerConnector serverConnector = new ServerConnector(new InetSocketAddress("localhost", 11111));
+        int port = 11111;
+
+        InetSocketAddress socketAddress;
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            socketAddress = new InetSocketAddress(inetAddress, port);
+        } catch (UnknownHostException e) {
+            socketAddress = new InetSocketAddress("localhost", port);
+        }
+
+        ServerConnector serverConnector = new ServerConnector(socketAddress);
         InputReader inputReader = new InputReader();
 
         inputReader.startReading(serverConnector);
